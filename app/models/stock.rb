ba7cdf2f1ce24208e197695789b1d34aec6fc579
Robert <constantinescu.robert94@gmail.com>
2021-08-15 20:01:03 +0300

    added stock model and IEX api client call encrypted IEX api key using ' EDITOR=vi bin/rails credentials:edit'

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,30 @@
 class Stock < ApplicationRecord
 
+  # before_action :iex_client_sandbox, only: [:new_lookup]
+  def iex_client
+    IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+  end
+
+  def iex_client_sandbox
+    IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token_sandbox],
+      secret_token: Rails.application.credentials.iex_client[:secret_token_sandbox],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+        )
+  end
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token_sandbox],
+      secret_token: Rails.application.credentials.iex_client[:secret_token_sandbox],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    client.quote(ticker_symbol).latest_price
+  end
+  # private_class_method :iex_client
 
 end
