2c0693f415b60ffe166d39b47e26c08318f50c0c
Robert <constantinescu.robert94@gmail.com>
2021-08-16 21:09:41 +0300

    added AJAX support to handle AJAX request

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,7 +5,11 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        # the respond to block of code, handles the AJAX requests
+        respond_to do |format|
+          # the partial expects a _stock_data.js.erb file under the views/users folder
+          format.js { render partial: 'users/stock_data'}
+        end
       else
         flash[:alert] = "Symbol not found: #{params[:stock]}"
         redirect_to my_portfolio_path
