18d83bc387b80b75fe623331f1f738d6ac5772b8
Robert <constantinescu.robert94@gmail.com>
2021-08-16 20:26:18 +0300

    - changes to stock model - added StockController - added some styling

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,10 @@
+class StocksController < ApplicationController
+
+
+  def search
+    @stock = Stock.new_lookup(params[:stock])
+    render 'users/my_portfolio'
+  end
+
+
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -23,7 +23,8 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex_client[:secret_token_sandbox],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.quote(ticker_symbol).latest_price
+    client.quote(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
   end
   # private_class_method :iex_client
 
