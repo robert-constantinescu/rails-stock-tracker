15d30d8fb800879bb43dfd5a2c7089edfe1ba72e
Robert <constantinescu.robert94@gmail.com>
2021-08-17 21:28:16 +0300

    edit user from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   end
 
   def full_name
-    return "#{irst_name} #{last_name}" if first_name || last_name
+    return "#{first_name} #{last_name}" if first_name || last_name
     'Anonymous'
   end
 end
